efbc86da79b92f9912c3eea3893f08c9ce49794c
Alexey_Svintitsky <Alexey.Svintitsky@gmail.com>
2026-09-03 19:55:55 +0300

    Доработка

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,29 +2,44 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage {
-    public final By userNameInput = By.cssSelector("#user-name");
-    public final By passwordNameInput = By.cssSelector("#password");
-    public final By loginBth = By.id("login-button");
 
-    WebDriver driver;
+    private final By userNameInput = By.cssSelector("#user-name");
+    private final By passwordInput = By.cssSelector("#password");
+    private final By loginButton = By.id("login-button");
+    private final By errorMessage = By.xpath("//h3[@data-test='error']");
+
+    private final WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
+
     public void open() {
         driver.get("https://www.saucedemo.com/");
     }
-    public void login(String user, String password) {
-        driver.findElement(userNameInput).sendKeys(user);
-        driver.findElement(passwordNameInput).sendKeys(password);
-        driver.findElement(loginBth).click();
+
+    public void login(String username, String password) {
+        driver.findElement(userNameInput).sendKeys(username);
+        driver.findElement(passwordInput).sendKeys(password);
+        driver.findElement(loginButton).click();
     }
+
     public boolean isErrorVisible() {
-        return driver.findElement(By.xpath("//h3[@data-test='error']")).isDisplayed();
+        return driver.findElement(errorMessage).isDisplayed();
     }
+
     public String getErrorText() {
-        return driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
+        return driver.findElement(errorMessage).getText();
+    }
+
+    public void waitForError() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage));
     }
 }
